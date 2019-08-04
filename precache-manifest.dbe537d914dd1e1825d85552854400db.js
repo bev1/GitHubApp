370ea81c852e1add5ad716f9b0edadec052ce7f4
bev1 <longer30001@gmail.com>
2019-08-04 20:53:32 +0300

    Updates

---
+++ precache-manifest.dbe537d914dd1e1825d85552854400db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbb10236ea77a045b532eaf543128ad",
+    "url": "/GitHubApp/index.html"
+  },
+  {
+    "revision": "4cd456184d8fd154801a",
+    "url": "/GitHubApp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0bf40a7c8bd8469500c8",
+    "url": "/GitHubApp/static/js/2.18691f6a.chunk.js"
+  },
+  {
+    "revision": "4cd456184d8fd154801a",
+    "url": "/GitHubApp/static/js/main.b53624a3.chunk.js"
+  },
+  {
+    "revision": "529a855e7038cc101f60",
+    "url": "/GitHubApp/static/js/runtime~main.81821662.js"
+  }
+]);
